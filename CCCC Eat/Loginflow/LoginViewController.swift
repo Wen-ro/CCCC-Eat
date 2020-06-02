8db687af2a02f17fd4480da3ea842f70aca4d1b1
張雅雯 <wen77797@gmail.com>
2020-06-02 16:43:52 +0800

    add alamorefire

--- CCCC Eat/Loginflow/LoginViewController.swift
+++ CCCC Eat/Loginflow/LoginViewController.swift
@@ -14,6 +14,56 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        AF.request("https://smartfood.azurewebsites.net/api/ProfileApi/2").responseString(completionHandler: {
+        response
+        in
+        
+        switch( response.result ){
+        case let .success(value) :
+        
+            //print(value)
+            //print(response.response!.statusCode)
+            
+            if let dataFromString = value.data(using: .utf8, allowLossyConversion: false) {
+                let json = try! JSON(data: dataFromString)
+                
+                //print(json)
+                
+                for (index,subJson):(String, JSON) in json {
+                    // Do something you want
+                    
+                    
+                    
+                    
+                    let name : String = subJson["name"].stringValue
+                    let metabolicRate : Double = subJson["metabolicRate"].doubleValue
+                    
+                    print("\( name ): \( metabolicRate )")
+                    
+                }
+                
+                
+            }
+            
+            
+            // 注意 guard 的使用方法。
+            guard 200 == response.response!.statusCode else {
+                
+                
+                
+                return
+            }
+            
+            
+            break;
+        case let .failure(error) :
+            
+            print(error.localizedDescription)
+            break;
+        }
+        
+        })
     }
     
 
--- Podfile
+++ Podfile
@@ -1,12 +1,13 @@
 # Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+ platform :ios, '12.0'
 
 target 'CCCC Eat' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
   # Pods for CCCC Eat
-
+  pod 'Alamofire', '~> 5.2'
+  
   target 'CCCC EatTests' do
     inherit! :search_paths
     # Pods for testing
